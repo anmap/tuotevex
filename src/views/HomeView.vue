785636247992ab17daec6d80f98e61696279469c
An Nguyen <josephus.an.nguyen@gmail.com>
2026-01-17 23:16:23 +0200

    Test fetching products on HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,20 @@
 <script setup lang="ts">
+import { useQuery } from '@tanstack/vue-query'
 import ProductCard from '@/components/ProductCard.vue'
-import { testProducts } from '@/data/testProducts'
+import { fetchProducts } from '@/services/productApi';
 
+// Fetch all products when no search query
+const { data: allProductsData, isLoading: isLoadingAllProducts, error: allProductsError } = useQuery({
+  queryKey: ['products'],
+  queryFn: fetchProducts,
+})
 </script>
 
 <template>
   <div class="container mx-auto px-4 py-8">
-    <h1 class="text-3xl font-bold mb-6">Products ({{ testProducts.length }})</h1>
+    <h1 class="text-3xl font-bold mb-6">Products ({{ allProductsData?.products.length }})</h1>
     <div class="space-y-4">
-      <ProductCard v-for="product in testProducts" :key="product.id" :product="product" />
+      <ProductCard v-for="product in allProductsData?.products" :key="product.id" :product="product" />
     </div>
   </div>
 </template>
