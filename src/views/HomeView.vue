cacfe85b25ff0591e8d4ed21d00db1beec53ba21
An Nguyen <josephus.an.nguyen@gmail.com>
2026-01-18 01:23:21 +0200

    Used ProductCardSkeleton in HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import { useQuery } from '@tanstack/vue-query'
 import ProductCard from '@/components/ProductCard.vue'
+import ProductCardSkeleton from '@/components/ProductCardSkeleton.vue'
 import { fetchProducts } from '@/services/productApi';
 
 // Fetch all products when no search query
-const { data: allProductsData, isLoading: isLoadingAllProducts, error: allProductsError } = useQuery({
+const { data: allProductsData, isLoading } = useQuery({
   queryKey: ['products'],
   queryFn: fetchProducts,
 })
@@ -12,9 +13,14 @@ const { data: allProductsData, isLoading: isLoadingAllProducts, error: allProduc
 
 <template>
   <div class="container mx-auto px-4 py-8">
-    <h1 class="text-3xl font-bold mb-6">Products ({{ allProductsData?.products.length }})</h1>
+    <h1 class="text-3xl font-bold mb-6">Products ({{ allProductsData?.products.length || 0 }})</h1>
     <div class="space-y-4">
-      <ProductCard v-for="product in allProductsData?.products" :key="product.id" :product="product" />
+      <template v-if="isLoading">
+        <ProductCardSkeleton v-for="n in 5" :key="n" />
+      </template>
+      <template v-else>
+        <ProductCard v-for="product in allProductsData?.products" :key="product.id" :product="product" />
+      </template>
     </div>
   </div>
 </template>
