4ffc39bc0d75222d263b2b447d48f8a0c52c1f80
An Nguyen <josephus.an.nguyen@gmail.com>
2026-01-18 20:59:15 +0200

    Added 2-way binding to SearchBar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import { Search } from 'lucide-vue-next'
+
+const searchValue = defineModel<string>()
 </script>
 
 <template>
-  <label for="search-input" class="sr-only">Search products</label>
   <div
     class="flex w-full items-center gap-2 rounded-full border border-gray-200 bg-white px-4 py-2 text-sm text-slate-900 shadow-sm transition focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2 focus-within:ring-offset-white">
+    <label for="search-input" class="sr-only">Search products</label>
     <Search class="h-4 w-4 text-slate-400" aria-hidden="true" />
-    <input id="search-input" type="search" name="search" placeholder="Search products" autocomplete="off"
-      class="w-full bg-transparent text-sm placeholder:text-slate-400 focus:outline-none" />
+    <input id="search-input" v-model="searchValue" type="search" name="search" placeholder="Search products"
+      autocomplete="off" class="w-full bg-transparent text-sm placeholder:text-slate-400 focus-visible:outline-none" />
   </div>
 </template>
