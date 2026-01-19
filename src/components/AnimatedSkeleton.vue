def4f7caad154fc26123bbeec61183aa21a43f30
An Nguyen <josephus.an.nguyen@gmail.com>
2026-01-19 13:15:17 +0200

    Changed to self-closed tag so it looks cleaner

--- src/components/AnimatedSkeleton.vue
+++ src/components/AnimatedSkeleton.vue
@@ -1,4 +1,3 @@
 <template>
-  <div aria-hidden="true" role="presentation" class="bg-linear-to-r from-gray-300 to-gray-200 animate-pulse rounded">
-  </div>
+  <div aria-hidden="true" role="presentation" class="bg-linear-to-r from-gray-300 to-gray-200 animate-pulse rounded" />
 </template>
