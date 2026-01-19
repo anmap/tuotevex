d61712dbe1f60f3fba4e7e4065edb63effb339d4
An Nguyen <josephus.an.nguyen@gmail.com>
2026-01-19 13:07:49 +0200

    Added role=presentation to Skeleton to mark as only visual styling

--- src/components/AnimatedSkeleton.vue
+++ src/components/AnimatedSkeleton.vue
@@ -1,5 +1,4 @@
 <template>
-  <div class="bg-linear-to-r from-gray-300 to-gray-200 animate-pulse rounded">
-    &nbsp;
+  <div aria-hidden="true" role="presentation" class="bg-linear-to-r from-gray-300 to-gray-200 animate-pulse rounded">
   </div>
 </template>
